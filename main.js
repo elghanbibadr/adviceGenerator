60ca8385ba9cd68aa2936998ec7f51d83a58042e
badr ghanbiE <bghaE@gmail.com>
2022-10-03 16:52:33 +0100

    handling event click

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // adding the variabels from the html document
 let idOfAdvice=document.querySelector('.idNumber');
 let adviceContent=document.querySelector('.advice__content');
+let iconDice=document.querySelector('.icon-dice');
+
+//  generateAdvice when clicking on the dice icon ; 
+iconDice.addEventListener('click',generateAdvice)
 
 // ² we need to fetch the api url and take the advice and its id
-
 function generateAdvice(){
-  let slip_id= randomIntFromInterval(100);
+  let slip_id= randomIntFromInterval(200);
     fetch(`https://api.adviceslip.com/advice/${slip_id}`)
     .then(response=>response.json())
     .then(data=>{
@@ -23,5 +26,8 @@ function randomIntFromInterval( max) { // min and max included
     return Math.floor(Math.random() * (max ) +1)
   }
 
- generateAdvice(); 
+  // Generate a random advice when the user first enter the page
+  generateAdvice();
+
+
 
